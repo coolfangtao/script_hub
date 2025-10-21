92c3745772a2fb67039be45eac4a6fdc1a2f534b
coolfangtao <843751829@qq.com>
2025-10-21 18:11:45 +0800

    代码优化

--- pages/test2.py
+++ pages/test2.py
@@ -39,7 +39,7 @@ def get_gemini_response(image, prompt):
     try:
         print(image)
         print("收到img")
-        model = genai.GenerativeModel('gemini-2.5-flash')
+        model = genai.GenerativeModel('gemini-2.0-flash-lite')
         print("等待响应中")
         response = model.generate_content([prompt, image], stream=True)
         print("已输出响应")
