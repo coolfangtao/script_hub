8cc36801540e3978c2239b1a1f4e94a2269647c4
coolfangtao <843751829@qq.com>
2025-10-25 03:01:37 +0800

    Listing生成代码优化

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from shared.sidebar import create_common_sidebar
-from shared.changelog import show_changelog
+from shared.update_log import show_changelog
 from shared.feedback import setup_database, show_feedback_module
 # --- 新增导入 ---
 from shared.auth import init_db, show_auth_page
