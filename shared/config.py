a13844d7ad1f18ae0833a97ac1059bc82246eb90
coolfangtao <843751829@qq.com>
2025-11-06 23:25:44 +0800

    去除文字功能开发

--- shared/config.py
+++ shared/config.py
@@ -39,6 +39,7 @@ class GlobalConfig:
             "gemini-2.0-flash-lite",  # 可用，9.62秒
             "gemini-2.5-flash",  # 可用，6.74秒
             "gemini-robotics-er-1.5-preview",  # 可用，8.73秒
+            "gemini-2.0-flash-preview-image-generation"
         ]
 
         # 定义时区
